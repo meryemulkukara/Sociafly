17ce8443c4b6f5a915e5e284f1861f011d4062a8
mulku <mkara@cyangate.com>
2025-06-13 22:18:35 +0300

    First commit

---
+++ sociafly/src/main/java/com/project/sociafly/SociaflyApplication.java
@@ -0,0 +1,13 @@
+package com.project.sociafly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SociaflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SociaflyApplication.class, args);
+	}
+
+}
