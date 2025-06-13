d0d658489bf2fa3cf97c323c71412d2faddc1a43
mulku <mkara@cyangate.com>
2025-06-13 22:18:35 +0300

    First commit

---
+++ sociafly/src/test/java/com/project/sociafly/SociaflyApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.project.sociafly;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SociaflyApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
